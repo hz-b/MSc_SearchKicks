76dd37966917542cc337dfd041659c3df1cae8c1
Olivier CHURLAUD <olivier@churlaud.com>
2015-10-30 14:38:43 +0100

    simplify ui, add menus

--- search_kicks/ui/main_window.py
+++ search_kicks/ui/main_window.py
@@ -8,6 +8,7 @@ from PyQt4.QtGui import QMainWindow, qApp
 from PyQt4 import uic
 
 from search_kicks.core.enumerations import OrbitSourceItems, DataSourceItems, AxisItems
+from search_kicks import core
 
 
 class MainWindow(QMainWindow):
@@ -29,31 +30,12 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self, parent)
         uic.loadUi('search_kicks/ui/main_window.ui', self)
         self.setWindowTitle(
-                qApp.applicationName() + " v" + qApp.applicationVersion())
+            qApp.applicationName() + " v" + qApp.applicationVersion()
+            )
         self.reference_orbit = 0
 
-        self.__set_orbite_source_combo()
-        self.__set_data_source_combo()
-        self.__set_axis_combo()
 
     # Slots
-    @pyqtSlot()
-    def on_set_ref_btn_clicked(self):
-        if self.orbite_source_combo.currentIndex() == OrbitSourceItems.current_orbit:
-            current_time = time.strftime("%d %B %Y, %H:%M:%S")
-            self.reference_orbit_text = "The reference orbit is the one "
-            + "at {:s}".format(current_time)
-
-        if self.orbite_source_combo.currentIndex() == OrbitSourceItems.time_signal:
-            self.reference_orbit_text = "The reference orbit is the orbit "
-            + "made out of the analysis of the {:d} "
-            + "Hz".format(self.frequency)
-
-        if self.orbite_source_combo.currentIndex() == OrbitSourceItems.load_orbit:
-            self.reference_orbit_text = "The reference orbit is the loaded "
-            + "orbit"
-
-        self.reference_orbite_text += " (BPM{:s})".format(self.properties.axis)
 
     @pyqtSlot()
     def on_reset_ref_btn_clicked(self):
@@ -62,41 +44,6 @@ class MainWindow(QMainWindow):
 
     @pyqtSlot()
     def on_execute_btn_clicked(self):
+        orbit, phase, tune = self.getOrbitData()
+        best_idx, phase_kick = core.get_kick(orbit, phase, tune)
         self.settings_clicked.emit()
-
-    @pyqtSlot()
-    def on_settings_btn_clicked(self):
-        self.execute_clicked.emit()
-
-    @pyqtSlot(int)
-    def on_orbit_source_combo_currentIndexChanged(self, index):
-        self.properties['orbit_source'] = index
-
-    @pyqtSlot(int)
-    def on_data_source_combo_currentIndexChanged(self, index):
-        self.properties['data_source'] = index
-
-    @pyqtSlot(int)
-    def on_axis_combo_currentIndexChanged(self, index):
-        self.properties['axis'] = index
-
-    # Private methods
-    def __set_orbite_source_combo(self):
-        self.orbite_source_combo.insertItem(OrbitSourceItems.current_orbit,
-                                            "Current orbit")
-        self.orbite_source_combo.insertItem(OrbitSourceItems.time_signal,
-                                            "Orbit out of the time signal")
-        self.orbite_source_combo.insertItem(OrbitSourceItems.load_orbit,
-                                            "Load orbit")
-
-    def __set_data_source_combo(self):
-        self.data_source_combo.insertItem(DataSourceItems.text_entry,
-                                          "Data from text entry")
-        self.data_source_combo.insertItem(DataSourceItems.file_entry,
-                                          "Data from file entry")
-
-    def __set_axis_combo(self):
-        self.axis_combo.insertItem(AxisItems.x,
-                                   "BPMx")
-        self.axis_combo.insertItem(AxisItems.y,
-                                   "BPMy")
