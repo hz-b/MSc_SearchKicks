2104d0e3b3dc66d6717c2868b00763fc1096a98d
Olivier CHURLAUD <olivier@churlaud.com>
2016-03-24 15:16:53 +0100

    draft...

--- search_kicks/core/build_sine.py
+++ search_kicks/core/build_sine.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def build_sine(kick_phase, tune, sin_coefficients):
+def build_sine(kick_phase, tune, sin_coefficients, phase=None):
     """ Build a sine with a kich at kick_phase
 
         Parameters
@@ -23,18 +23,23 @@ def build_sine(kick_phase, tune, sin_coefficients):
             Phase build with a linspace
 
     """
-
-    phase_tmp = np.linspace(kick_phase, kick_phase+tune*2*np.pi, 5000)
-    phase_tmp = phase_tmp[:-1]  # The last point is the same as the first one
+    if phase is None:
+        phase_tmp = np.linspace(0, tune*2*np.pi, 5000)
+        phase_tmp = phase_tmp[:-1]  # The last point is the same as the first one
+    else:
+        phase_tmp = phase % tune*2*np.pi
+#        kick_id = np.where((phase-kick_phase) >= 0)[0][0]
+#        phase_tmp = np.concatenate((phase[kick_id:], phase[:kick_id]+tune*2*np.pi))
+       # print(phase_tmp)
 
     b = sin_coefficients[0]
     c = sin_coefficients[1]
 
-    sine_tmp = np.concatenate((b*np.sin(phase_tmp + c),
-                               b*np.sin(phase_tmp + c)))
+    sine_tmp = np.concatenate((b*np.sin(phase_tmp + kick_phase + c),
+                               b*np.sin(phase_tmp + kick_phase + c)))
 
     phase_exp = np.concatenate((phase_tmp-2*np.pi*tune,
-                                phase_tmp))
+                                phase_tmp))+kick_phase
 
     valid_ids = np.logical_and(phase_exp >= 0, phase_exp <= tune*2*np.pi)
 
