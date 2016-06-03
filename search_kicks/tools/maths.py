3eae8c1462913534c038c5c635114272a9158bc9
Olivier CHURLAUD <olivier@churlaud.com>
2016-06-03 16:03:00 +0200

    correct svd for pyhton 3

--- search_kicks/tools/maths.py
+++ search_kicks/tools/maths.py
@@ -249,8 +249,12 @@ def inverse_with_svd(M, nb_values):
 
     """
 
-    U, s, V = np.linalg.svd(M.newbyteorder('='), full_matrices=False)
-    #U, s, V = np.linalg.svd(M, full_matrices=False)
+    try:
+        M = M.newbyteorder('=')
+    except AttributeError:
+        pass
+
+    U, s, V = np.linalg.svd(M, full_matrices=False)
     # S_mat = U * diag(s) * V
     idmax = nb_values
     Sred = np.diag(np.ones(idmax)/s[:idmax])
