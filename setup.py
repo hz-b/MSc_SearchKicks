e4bc2e700c9cf4316f5f88c6eeab3e16f620a792
Olivier CHURLAUD <olivier@churlaud.com>
2016-08-07 11:03:52 +0200

    add license

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 setup(name = 'search_kicks',
       version = '0.1.0',
       maintainer = 'Olivier Churlaud',
-      maintainer_email= 'olivier.churlaud@helmholtz-berlin.de',
+      maintainer_email= 'olivier@.churlaud.com',
       description = 'Various tools to find kicks and work on orbit data',
-      url = 'aragon.acc.bessy.de:/opt/repositories/controls/git/tools/search_kicks.git',
+      url = 'https://github.com/ochurlaud/MSc_SearchKicks',
       package_dir = {'search_kicks': 'search_kicks'},
       packages=find_packages(),
     )
